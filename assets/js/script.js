b567b276264211df5a2e672ca6de76a610d4539f
Dan Høegh <dan.hoegh@gmail.com>
2022-03-31 11:20:27 +0200

    stuff

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,26 @@
 // JS by Dan Høegh
 // UCN MMD 2021
+
+let visitedCount = 0;
+let localValue = localStorage.getItem("visitedAmount");
+
+const updateCountOnPage = () => {
+    document.querySelector("#app").innerHTML = visitedCount;
+}
+
+if (localValue == null || localValue == ""){
+    localStorage.setItem("visitedAmount", "0");
+} else {
+    visitedCount = localStorage.getItem("visitedAmount");
+}
+visitedCount++;
+localStorage.setItem("visitedAmount", visitedCount);
+updateCountOnPage();
+document.querySelector("body").classList.add("visited" + visitedCount);
+
+document.querySelector("#resetVisitedAmount")
+.addEventListener("click", () => {
+    localStorage.setItem("visitedAmount", "0");
+    visitedCount = 0;
+    updateCountOnPage();
+});
